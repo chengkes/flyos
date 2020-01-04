775113dc14ab1070484bf43cdf82432ae8de5405
chengkes <chengkks1987n@126.com>
2020-01-04 11:37:41 +0800

    ok

--- src/boot.asm
+++ src/boot.asm
@@ -1,6 +1,6 @@
 org	07c00h			; 告诉编译器程序加载到7c00处
 
-		; 下面是 FAT12 磁盘的头
+	; 下面是 FAT12 磁盘的头
 	jmp LABEL_START
 	nop
 	BS_OEMName	DB 'ForrestY'	; OEM String, 必须 8 个字节
@@ -30,78 +30,68 @@ LABEL_START:
 	mov	ss, ax
 	mov	sp, 07c00h
  
-	mov	ax, BootMessage 
 	push 	BootMessage
+	push	0103h
 	call	DispStr			; 调用显示字符串例程
-	and 	sp, 2
+	add 	sp, 4	
 	
-	;; find file loader.bin
-	
-	;; FAT12 根目录区长度
-	
+	; 复位软驱
+        xor 	ah, ah
+        xor 	dl, dl                  ;驱动器号 A盘
+        int 	13h
 
+	; 读取软盘扇区数据
+	push	1
+	push	0
+	push	cs
+	push	09000h
+	call	ReadSector
+	add 	sp, 8	
+	
 	jmp	$			; 无限循环
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 显示字符串，字符串长度必须为10
+;; 参数1：word，串地址
+;; 参数2：word，显示位置，高位=行，低位=列
 DispStr:
-	push	ax
-	push	bp
-	push	cx
-	push	dx
 	mov	bp,sp
-	mov	ax, [bp+10] 
-	mov	bp, ax			; ES:BP = 串地址
+	mov	dx, [ss:bp+2]   	;(DH、DL)＝坐标(行、列)
+	mov	bp, [ss:bp+4]   	; ES:BP = 串地址
 	mov	cx, 10			; CX = 串长度
 	mov	ax, 01301h		; AH = 13,  AL = 01h
 	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
-	mov	dl, 0
-	int	10h			; 10h 号中断
- 
-	pop	dx
-	pop	cx
-	pop	bp
-	pop	ax
+	int	10h			; 10h 号中断 	
 	ret 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 读入软盘扇区数据到内存 
+;; 参数1， word， 读取扇区数目, 低位有效
+;; 参数2， word， 开始扇区号
+;; 参赛3， word,  内存地址，段基址
+;; 参赛4， word,  内存地址，段内偏移
+ReadSector: 	 
+	mov	bp, sp	
+	mov	bx, [ss:bp+2]	; ES:BX=缓冲区的地址  
+	mov	es, [ss:bp+4]	
+	mov	ax, [ss:bp+8]	; AL=读取扇区数目
 	
-	; 复位软驱
-	xor ah, ah	
-	xor dl, dl			;驱动器号 A盘
-	int 13h
 	
-	; 读取软盘扇区数据
-	mov ax, 19		; 
-	mov bl, 18
-	div bl
-	inc ah	
-	mov  cl, ah				;起始扇区号
-	mov dh,  al			
-	and dh, 1				;磁头号
-	shr al, 1
-	mov ch,	al			;磁道号
-	mov ah, 02h	
-	xor dl, dl			;驱动器号 A盘
-	mov al, 1 			; 要读扇区数
-	mov bx, 8000h
-	int 13h
+	;div 	[BPB_SecPerTrk]
 	
-	mov si, LOADER_NAME
-	mov di, bx
-	.goon:
-	dec al
-	test al, al
-	cmps	
-	jz   .goon
-	mov al, 11
-	sub si, LOADER_NAME
-	sub di, si
-	mov si, LOADER_NAME
-	add di, 32
-	jmp  .goon
+        mov     ch, 0            ; 柱面
+        mov     cl, 0            ; 扇区
+        mov     dh, 0            ; 磁头
+ 		        
+	xor     dl,dl           ; 驱动器号
+        mov     ah, 02h         ; 功能号，读扇区        
+        int     13h	
+		 
+	ret
 
 
-	jmp	$			; 无限循环
-
-LOADER_NAME:  db "loader  bin"
-BootMessage:		db	"Hello,FLY world!"
+LOADER_NAME: 		db 	"loader  bin"
+BootMessage:		db	"Hello,FLY !"
 times 	510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
 dw 	0xaa55				; 结束标志
+
--- src/run/flyrun.sh
+++
@@ -1,6 +0,0 @@
-cd /home/cklj/reps/flyos/src
-
-nasm boot.asm -o  ./run/boot.bin 
-cd ./run
-dd if=boot.bin of=flyos.img conv=notrunc
-bochs -q -f flyos.bochsrc -rc flyos.debug
